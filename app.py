6cb0ab84efe91d4e4588d89e0b7676e0ba9f3848
Vishal Dalmiya <dalmiya2@illinois.edu>
2017-11-13 21:28:03 -0800

    1) added title to the description 2) added logic to remove the "idx" folder

--- app.py
+++ app.py
@@ -1,44 +1,39 @@
 from flask import Flask, request, render_template
 import walmart as wm
-import createIndex as ci
+import threading
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+# app.config['DEBUG'] = True
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-#test
-# @app.route("/results", methods=['post'])
-# def results():
-#	search_term = request.form['search']
-#	return render_template('results.html',s_t = search_term)
-
-
 @app.route("/results", methods=['post'])
+
+
 def results():
-    print('hi')
     search_term = request.form['search']
-    print(search_term)
-    wal = wm.Walmart()
-    res = wal.get_search_results(search_term)
-    final_res = ci.createIndex(search_term)
-    final_res_id = [item[0] for item in final_res]
-    results = [res[i] for i in final_res_id]
-    print(len(results))
+    # threading._start_new_thread(compute)
+
+    search_term = "basket"
+    walmart = wm.Walmart()
+    results = walmart.get_search_results(search_term)
+    walmart.write_results(results)
+    rank_res = walmart.createIndex(search_term)
+    print(rank_res)
+
     head1 = '<h1> Search Results for: ' + search_term + '</h1><br><br>'
-    # head2 = '<br>'
     tbl_start = '<table><tr><th>Product</th><th>Review</th><th>Sentiment</th><th width="200"></th></tr>'
     tbl_end = '</table>'
     tbl_data = ''
-    for i, result in enumerate(results):
-        # head2 = head2+'<h2>' +result+ '&nbsp&nbsp&nbsp&nbsp'+ sentiments[i] +'</h2>'
-        tbl_data = tbl_data + '<tr><td>' + results[i].desc + '</td><td>' + results[i].review + '</td><td>' + results[
-            i].sentiment + '</td><td width="200"><a href="' + results[i].search_url + '">Buy Now</a></td></tr>'
+    for i, result in enumerate(rank_res):
+        tbl_data = tbl_data + '<tr><td>' + rank_res[i].desc + '</td><td>' + rank_res[i].review + '</td><td>' + rank_res[
+            i].sentiment + '</td><td width="200"><a href="' + rank_res[i].search_url + '">Buy Now</a></td></tr>'
 
     return head1 + tbl_start + tbl_data + tbl_end
 
 
-app.run(debug=True)
+# app.run(debug=True)
+app.run()
