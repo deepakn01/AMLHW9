9cb41da994238c028ad461df38b5d57989579eeb
Dalmiya Vishal <dalmiya2@illinois.edu>
2017-11-14 05:52:31 +0000

    Delete testCI.py

--- testCI.py
+++
@@ -1,9 +0,0 @@
-import createIndex as ci
-
-search_term = 'basketball'
-
-if __name__ == '__main__':
-        #CI = ci.createIndex()
-        res = ci.createIndex(search_term)
-        res2 = [item[0] for item in res]
-        print(res2)
