5e91fd0b5074009bfc2ab56ead090c563a19e54e
Vishal Dalmiya <dalmiya2@illinois.edu>
2017-12-02 15:28:02 -0800

    Separated SearchResult in a separate file

---
+++ SearchResult.py
@@ -0,0 +1,7 @@
+class SearchResult:
+    def __init__(self, search_url, title, desc, review, sentiment):
+        self.search_url = search_url
+        self.title = title
+        self.desc = desc
+        self.review = review
+        self.sentiment = sentiment
