a98635c0fb25224cc510b92cb096a0e773b9da2a
dnagarajan <deepakn2@illinois.edu>
2017-12-04 20:48:19 -0500

    Fixed - Rocchio update; cleaned up the code

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session, g
 import walmart as wm
 import kohls as kh
 import rank
 import os
+import SearchResult as search_res
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
 rank.Rank.deletefile()
+search_res = []
+
+
 
 @app.route("/", methods=['get','post'])
 
 def index():
+    global search_res
     return render_template('index.html')
 
-@app.route("/results", methods=['post'])
+@app.route("/results", methods=['get','post'])
 
 def results():
+    global search_res
     query_txt = request.form['search']
+    session['query_txt'] = query_txt
+
     walmart = wm.Walmart()
     search_res = walmart.get_search_results(query_txt)
 
@@ -30,41 +41,32 @@ def results():
     rank_res = ranker.score(inv_idx, query, 10)
     print(rank_res)
 
-    # write the result for Rocchio fedback
-    # todo: take the user click
-    #rank.Rank.write_rocchio_feedback(3)
 
     rocchio_res = rank.Rank.get_rocchio_ranking(query, ranker, inv_idx)
     print(rocchio_res)
 
-    if (os.stat("cranfield-qrels.txt").st_size == 0):
-        results = rank_res
-    else:
-        results = rocchio_res
+    results = rank_res
 
-    head1 = '<h1> Search Results for: ' + query_txt + '</h1><br><br>'
-    tbl_start = '<table><tr><th>Product</th><th>Description</th><th>Review</th><th>Sentiment</th><th>Website</th></tr>'
-    tbl_end = '</table>'
-    tbl_data = ''
-    btn = '<br><br><br><center> <form action="/" method="post"><input type="submit" name="submit" value="Refine Query?"></form></center>'
-    for i, result in enumerate(results):
-        inv_idx = result[0]
-        res_url = str(search_res[inv_idx].search_url)
-        print(inv_idx)
-        tbl_data = tbl_data + '<tr><td>' + search_res[inv_idx].title + '</td><td>' + search_res[inv_idx].desc + '</td><td>'+ search_res[inv_idx].review + '</td><td>' + search_res[
-            inv_idx].sentiment + '</td><td>' + search_res[inv_idx].website + '</td><td width="400"><a href="/rocchio/' + str(inv_idx) + '/' + res_url.replace("/","|") + '" name="link" >Buy Now</a></td></tr>'
+    return render_template("results.html", results = results, query_txt = query_txt, search_res = search_res)
 
-    return head1 + tbl_start + tbl_data + tbl_end + btn
-
-@app.route("/rocchio/<i>/<u>", methods=['get','post'])
-def rocchio_results(i,u):
+@app.route("/rocchio/<i>/<path:u>", methods=['get','post'])
+def rocchio_write(i,u):
     print(request.method)
     print("Link is clicked")
     print(i)
     rank.Rank.write_rocchio_feedback(i)
     print(u)
-    u.replace("|","/")
-    print(u)
-    return redirect(u.replace("|","/"))
+    return redirect(u)
+
+@app.route("/rocchioresults/", methods=['get','post'])
+def rocchioresults():
+    global search_res
+    query_txt = session['query_txt']
+    inv_idx = rank.Rank.create_inv_idx()
+    query = rank.Rank.create_query_obj(query_txt)
+    ranker = rank.Rank.get_ranker(query_txt, inv_idx)
+    results = rank.Rank.get_rocchio_ranking(query, ranker, inv_idx)
+
+    return render_template("rocchioresults.html", results=results, query_txt=query_txt, search_res=search_res)
 
 app.run()
