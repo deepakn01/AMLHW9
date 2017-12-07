27a61602b93c2404b273515e3317ec1546532ff7
Vishal Dalmiya <dalmiya2@illinois.edu>
2017-12-06 22:38:12 -0800

    Code clean up

--- kohls.py
+++ kohls.py
@@ -1,15 +1,9 @@
 import requests
-from bs4 import BeautifulSoup as BS
+import bs4
 import re
 import SearchResult
-# from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# todo: separate topic with weightage
-# todo: create interface
-# todo: add desc to app display
-# todo: add more reviews
-# todo: use beautiful soup instead of regex
 
 class Kohls:
     def __init__(self):
@@ -17,7 +11,7 @@ class Kohls:
         self.base_url = "https://www.kohls.com"
 
     def get_search_url(self, url):
-        soup = BS(requests.get(url).text, "html.parser")
+        soup = bs4.BeautifulSoup(requests.get(url).text, "html.parser")
         pretty_text = soup.prettify()
         match = re.findall('"(/product.*?jsp)"', pretty_text)
 
@@ -25,7 +19,7 @@ class Kohls:
         unique = []
         for entry in match:
             m = re.match('(.*?jsp)', entry)
-            if(m.group() not in unique):
+            if (m.group() not in unique):
                 unique.append(m.group())
 
         return unique
@@ -38,29 +32,27 @@ class Kohls:
         return soup.title.string
 
     def get_review_title(self, soup):
-        review_all = soup.find_all("span", attrs={"itemprop":"name"})
+        review_all = soup.find_all("span", attrs={"itemprop": "name"})
         review_title = ''
         for review in review_all:
             review_title += ' ' + review.text
         return review_title
 
     def get_sentiment(self, text):
-        # blob = TextBlob(text)
-        # return blob.sentiment.polarity
         analyzer = SentimentIntensityAnalyzer()
         return str(analyzer.polarity_scores(text))
 
-    def get_search_results(self, search_str, count = 15):
+    def get_search_results(self, search_str, count=15):
         res = list()
         match = self.get_search_url(self.search_url + search_str)[0:count]
         for link in match:
             try:
                 url = self.base_url + link
-                soup = BS(requests.get(url).text, "html.parser")
+                soup = bs4.BeautifulSoup(requests.get(url).text, "html.parser")
 
                 title = self.get_title(soup)
                 desc = self.get_desc(soup)
-                review =  self.get_review_title(soup)
+                review = self.get_review_title(soup)
                 sentiment = self.get_sentiment(review)
                 print('link: ' + url)
                 print('title: ' + title)
@@ -75,38 +67,10 @@ class Kohls:
 
         return res
 
-# search_term = "nike shoes"
-# wm = Kohls()
-# results = wm.get_search_results(search_term)
-# wm.write_results(results)
-# rank_res = wm.createIndex(search_term)
+## Test code
+# search_term = "basket"
+# kh = Kohls()
+# results = kh.get_search_results(search_term)
+# kh.write_results(results)
+# rank_res = kh.createIndex(search_term)
 # print(rank_res)
-
-# to get the description of the product
-# res = soup.find_all(class_ = re.compile('description.*body'))
-# res = soup.find(class_ = re.compile('about.*desc')).contents
-
-
-# to get the search results
-# search_res = soup.find_all(class_ = re.compile('product.*title.*link'))
-# search_res = soup.find_all("a", class_="product-title-link")
-# search_res = soup.find_all(class_ = 'product-title-link')
-# search_res = soup.find_all(string = 'productPageUrl')
-# search_res = soup.find_all('a')
-
-
-# to get the links in the search page
-# for link in search_res:
-#     print(link.get('href'))
-
-# to get the title
-# print soup.title.string
-# print text
-
-# search by a string
-# res = soup.find_all(string = re.compile('Better'))
-
-# from pyquery import PyQuery as pq
-# name = 'basket'
-# response = pq(url=url.format(name=name))
-# print response
